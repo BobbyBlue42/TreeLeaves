bd00994ab6dee4b10d8a71c4afb37edb17285b4d
Louis Thie <louisthie@orcon.net.nz>
2017-07-27 01:02:19 +1200

    Integrate the TreeLeaves Custom Vision project

--- TreeLeaves/TreeLeaves/CustomVision.xaml.cs
+++ TreeLeaves/TreeLeaves/CustomVision.xaml.cs
@@ -1,11 +1,15 @@
-﻿using Plugin.Media;
+﻿using Newtonsoft.Json;
+using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-
+using TreeLeaves.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -44,7 +48,59 @@ namespace TreeLeaves
                 return file.GetStream();
             });
 
-            file.Dispose();
+            TagLabel.Text = "Analysing...";
+
+            await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+        async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Prediction-Key", "6a42d857aa574219aa5a2c6ef6e32282");
+
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/5f73d24b-1153-490c-a7f8-da544c4fd659/image";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+                    /*double max = responseModel.Predictions.Max(m => m.Probability);
+
+                    TagLabel.Text = (max >= 0.5) ? "Hotdog" : "Not hotdog";*/
+                    string tag = "No data on this type of leaf";
+                    double max = 0.0;
+                    foreach (Prediction prediction in responseModel.Predictions)
+                    {
+                        if (prediction.Probability > max && prediction.Probability >= 0.5)
+                        {
+                            tag = prediction.Tag;
+                        }
+                    }
+
+                    TagLabel.Text = tag;
+                }
+
+                file.Dispose();
+            }
         }
     }
 }
